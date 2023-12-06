72498b94355130086d6a8b838969d9a11b921e08
Kelsey Barham <kelseylbarham@gmail.com>
2023-12-06 09:14:01 -0500

    added loading page

--- trivia-game/src/App.jsx
+++ trivia-game/src/App.jsx
@@ -4,15 +4,18 @@ import axios from 'axios'
 
 function App() {
   const [categoryList, setCategoryList] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     axios
       .get('https://opentdb.com/api_category.php')
-      .then((res) => {setCategoryList(res.data.trivia_categories)
+      .then((res) => {
+        setLoading(false)
+        setCategoryList(res.data.trivia_categories)
       console.log(res.data)})
   }, [])
 
-  console.log('render runs')
+
   return (
     <div>
       <h1>React Trivia Game</h1>
