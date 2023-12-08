8e3732e9db6479fa453ee31fad1aeebbc777ad35
Kelsey Barham <kelseylbarham@gmail.com>
2023-12-08 15:09:03 -0500

    got individual questions showing up after category has been selected, added a loading page when data is being fetched, added previous and next buttons, started on a scoring system but still needs work

--- trivia-game/src/components/quiz-page.jsx
+++ trivia-game/src/components/quiz-page.jsx
@@ -10,45 +10,95 @@ const QuizPage = ({
     categoryID, 
 }) => { 
     const [questions, setQuestions] = useState([])
+    /**
+     * const questions = [];
+     * const setQuestions = ( updatedValue ) {
+     *  questions = updatedValue 
+     * }
+     * 
+     * const state = [question,setQuestions];
+     * const [questions,setQuestions] = state;
+     * 
+     * const [questions, setQuestions] = useState([])
+     * 
+     * const quetionState = useState([]) <- returns an array
+     * questionsState[0] //questions
+     * questionState[1] //setQuestions 
+     * 
+     * questionState = [questions, setQuestions]
+     * 
+     * 
+     */
+
+
+
+
+    const [index , setIndex] = useState(0)
+    const [loading, setLoading] = useState(true)
+    const [questionObjectIndex, setQuestionObjectIndex] = useState(0)
+    const [score , setScore] = useState(0)
 
     const isQuestionEmpty = questions.length === 0;
 
-    const [questionObjectIndex, setQuestionObjectIndex] = useState(0);
-
     const handleNextQuestion = () => {
         if (selectedAnswer === props.data[questionObjectIndex].correct_answer) {
-            console.log(co)
+        console.log()
         }
     }
 
 
     const advanceQuestion = () => {
-        setQuestionObjectIndex(
-        questionObjectIndex === questions.length - 1
-        ? questionObjectIndex
-        : questionObjectIndex + 1
+        setIndex(
+        index === questions.length - 1
+        ? index
+        : index + 1
     );
+    }
+
+    const previousQuestion = () => {
+        setIndex(
+        index ===  0
+        ? index
+        : index - 1
+    );
+    }
+
+    const recordAnswer = (event) => {
+        const answer = event.target.value
+        setChosenAnswer(answer)
+        if(answer === quiz[index].correct_answer) {
+            setScore(score + 1)
+        }
     }
 
 
     useEffect(() => {
         axios
             .get(`https://opentdb.com/api.php?amount=10&category=${categoryID}`)
-            .then((res) => setQuestions(res.data.results))
-    }, [])
+            .then((res) => {
+                setLoading(false)
+                setQuestions(res.data.results)})
+    }, [categoryID])
+
+
+    if (loading) {
+        return <h1> Page loading ⏳</h1>
+      }
 
     return (
         <div>
             <div className="quiz-page">
-                {questions.map((quiz) => (
+                <div className="display">{}</div>
                     <Quiz 
-                        key={quiz.id}
-                        question={quiz.question}
-                        correctAnswer={quiz.correct_answer}
-                        incorrectAnswers={quiz.incorrect_answers}
+                        key={questions[index].id}
+                        question={questions[index].question}
+                        correctAnswer={questions[index].correct_answer}
+                        incorrectAnswers={questions[index].incorrect_answers}
                     />
-                ))}
             </div>
+            {index > 0 && <button onClick={previousQuestion}>Previous Question</button>}
+            {index <= questions.length -1 && <button onClick={advanceQuestion}>Next Question</button>}
+
         </div>
     )
 }
@@ -58,14 +108,24 @@ function Quiz({question,correctAnswer,incorrectAnswers}) {
     //let answers creates the box for answers and the ... spreads the incorrect answers so they can be in an array with the correct answer. It's copying what's in the incorrect answer array into a new array called answers and also including the correct answer as the last element.
     let answers = [...incorrectAnswers, correctAnswer];
     answers = shuffle(answers)
+
+    const [score , setScore] = useState(0)
+
+
     const selectedAnswer = (value) => {
         //handle the clicking of answers in here
         if(value === correctAnswer) {
-            window.alert('YOU GOT IT RIGHT')
+            setScore(score + 1)
+            window.alert(`YOU GOT IT RIGHT. Your Score is ${score}`)
         } else {
         window.alert('Wrongo')
         }
     }
+
+    const allAnswers = [];
+    // if (props.data.length > 0) {
+    //     allAnswers.push();
+    // }
 
     return (
         <div>
@@ -73,6 +133,8 @@ function Quiz({question,correctAnswer,incorrectAnswers}) {
             <ul> 
             {answers.map((answer,idx)=>{ 
                    return <li onClick={()=> selectedAnswer(answer)} key={idx}><p >{answer}</p></li>
+
+                   //create a next button?
             })
             }
             </ul>
